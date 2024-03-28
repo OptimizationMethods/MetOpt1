05e230da62b8673ef5c334e460280955f2c2cb48
armantovmasyan <arman-nn04@mail.ru>
2024-03-28 18:15:50 +0300

    feat: major changes
    - Add requirements.txt file with dependencies
    - add README.md
    - grad.py, main.py
        - Tabs and spaces between objects refactor, based on PEP regulations
        - Add docstrings for every function (main.py in future)
        - Type hinting for args and return values

--- grad.py
+++ grad.py
@@ -1,73 +1,115 @@
 import time
 import numpy as np
 
-def golden_section_search(f, eps):
-    phi = (1 + np.sqrt(5)) / 2
-    phi2 = 2 - phi
-    left = 0
-    right = 1
-    while abs(right - left) > eps:
-        x1 = left + phi2 * (right - left)
-        x2 = right - phi2 * (right - left)
-
-        f_new1 = f(x1)
-        f_new2 = f(x2)
-
-        if f_new1 < f_new2:
-            right = x2
-        else:
-            left = x1
-
-    return (left + right) / 2
-
-def ternary_search(f, eps):
-    left = 0
-    right = 1
-    while abs(right - left) > eps:
-        m1 = left + (right - left) / 3
-        m2 = right - (right - left) / 3
-
-        f1 = f(m1)
-        f2 = f(m2)
-
-        if f1 < f2:
-            right = m2
-        else:
-            left = m1
-
-    return (left + right) / 2
+from typing import Callable, Optional
+from numpy.typing import NDArray
+from numbers import Real
 
 
-def gradient_descent(f, grad, x0, y0, selection_method, eps, learning_rate=None, max_iter=1500):
-    x_prev = x0
-    y_prev = y0
-    trajectory = [(x0, y0)]
+def golden_section_search(f: Callable[[Real], Real], eps: Real) -> Real:
+	"""
+	Golden section based search algorithm
 
-    iter = 0
+	:param f: investigated function
+	:param eps: precision
+	:return: minimum of function
+	"""
+	phi = (1 + np.sqrt(5)) / 2
+	phi2 = 2 - phi
+	left = 0
+	right = 1
+	while abs(right - left) > eps:
+		x1 = left + phi2 * (right - left)
+		x2 = right - phi2 * (right - left)
 
-    start_time = time.time()
-    x = x_prev
-    y = y_prev
-    for _ in range(max_iter):
-        grad_f = grad(x_prev, y_prev)
-        if learning_rate is not None:
-            alpha = learning_rate
-        else:
-            f_alpha = lambda alpha: f(x_prev - alpha * grad_f[0], y_prev - alpha * grad_f[1])
-            alpha = selection_method(f_alpha, eps)
-        x = x_prev - alpha * grad_f[0]
-        y = y_prev - alpha * grad_f[1]
+		f_new1 = f(x1)
+		f_new2 = f(x2)
 
-        trajectory.append((x, y))
+		if f_new1 < f_new2:
+			right = x2
+		else:
+			left = x1
 
-        iter += 1
+	return (left + right) / 2
 
-        if abs(f(x, y) - f(x_prev, y_prev)) < eps:
-            break
-        x_prev = x
-        y_prev = y
 
-    end_time = time.time()
-    exec_time = end_time - start_time
+def ternary_search(f: Callable[[Real], Real], eps: Real) -> Real:
+	"""
+	Ternary search for function minimum
 
-    return x, y, iter, exec_time, trajectory
+	:param f: investigated function
+	:param eps: precision
+	:return: minimum of function
+	"""
+	left = 0
+	right = 1
+	while abs(right - left) > eps:
+		m1 = left + (right - left) / 3
+		m2 = right - (right - left) / 3
+
+		f1 = f(m1)
+		f2 = f(m2)
+
+		if f1 < f2:
+			right = m2
+		else:
+			left = m1
+
+	return (left + right) / 2
+
+
+def gradient_descent(
+		f: Callable[[Real, ...], Real],
+		grad: Callable[[Real, ...], NDArray[Real, ...]],
+		x0: Real,
+		y0: Real,
+		selection_method: Callable[[Callable, Real], Real],
+		eps: Real,
+		learning_rate: Optional[Real] = None,
+		max_iter: int = 1500
+):
+	"""
+	Implements the gradient descent optimization algorithm.
+
+	:param f: Function that the algorithm is trying to minimize.
+	:param grad: Function that computes the gradient of 'f'.
+	:param x0: Initial x-coordinate of the point from which the algorithm starts the search.
+	:param y0: Initial y-coordinate of the point from which the algorithm starts the search.
+	:param selection_method: Method for selecting the step size.
+	:param eps: A real number representing the precision of the solution. The algorithm stops when the absolute difference between the function values at two consecutive iterations is less than 'eps'.
+	:param learning_rate: An optional real number representing the step size at each iteration. If not provided, the 'selection_method' is used to determine the step size.
+	:param max_iter: Maximum number of iterations.
+	:return:
+	"""
+	x_prev = x0
+	y_prev = y0
+	trajectory = [(x0, y0)]
+
+	iter_count = 0
+
+	start_time = time.time()
+	x = x_prev
+	y = y_prev
+	for _ in range(max_iter):
+		grad_f = grad(x_prev, y_prev)
+		if learning_rate is not None:
+			alpha = learning_rate
+		else:
+			f_alpha = lambda al: f(x_prev - al * grad_f[0], y_prev - al * grad_f[1])
+			alpha = selection_method(f_alpha, eps)
+		x = x_prev - alpha * grad_f[0]
+		y = y_prev - alpha * grad_f[1]
+
+		trajectory.append((x, y))
+
+		iter_count += 1
+
+		if abs(f(x, y) - f(x_prev, y_prev)) < eps:
+			break
+		x_prev = x
+		y_prev = y
+
+	end_time = time.time()
+	exec_time = end_time - start_time
+
+	return x, y, iter_count, exec_time, trajectory
--- main.py
+++ main.py
@@ -1,104 +1,128 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from grad import gradient_descent, golden_section_search, ternary_search
 from scipy.optimize import minimize
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-eps = 1e-12
 
-def f1(x, y):
-    return x ** 2 + y ** 2
+from numbers import Real
+from numpy.typing import NDArray
 
-def f2(x, y):
-    return x ** 2 * y ** 2 * np.log(8 * x**2 + 3 * y**2)
+EPS: Real = 1e-12
 
-def grad_f1(x, y):
-    df_dx = 2*x
-    df_dy = 2*y
-    return np.array([df_dx, df_dy])
 
-def grad_f2(x, y):
-    df_dx = 2 * x * y ** 2 * (np.log(8 * x ** 2 + 3 * y ** 2) + 8 * x ** 2 / (8 * x ** 2 + 3 * y ** 2))
-    df_dy = 2 * x ** 2 * y * (np.log(8 * x ** 2 + 3 * y ** 2) + 3 * y ** 2 / (8 * x ** 2 + 3 * y ** 2))
-    return np.array([df_dx, df_dy])
+def f1(x: Real, y: Real) -> Real:
+	return x ** 2 + y ** 2
 
-x0 = 0.1
-y0 = 0.1
-learning_rate = 0.1
 
-x_neopt1, y_neopt1, num_iterations_neopt1, execution_time_neopt1, trajectory_neopt1 = gradient_descent(f1, grad_f1, x0, y0, golden_section_search, eps, learning_rate)
+def f2(x: Real, y: Real) -> Real:
+	return x ** 2 * y ** 2 * np.log(8 * x ** 2 + 3 * y ** 2)
+
+
+def grad_f1(x: Real, y: Real) -> NDArray[Real, ...]:
+	df_dx = 2 * x
+	df_dy = 2 * y
+	return np.array([df_dx, df_dy])
+
+
+def grad_f2(x: Real, y: Real) -> NDArray[Real, ...]:
+	df_dx = 2 * x * y ** 2 * (np.log(8 * x ** 2 + 3 * y ** 2) + 8 * x ** 2 / (8 * x ** 2 + 3 * y ** 2))
+	df_dy = 2 * x ** 2 * y * (np.log(8 * x ** 2 + 3 * y ** 2) + 3 * y ** 2 / (8 * x ** 2 + 3 * y ** 2))
+	return np.array([df_dx, df_dy])
+
+
+x0: Real = 0.1
+y0: Real = 0.1
+learning_rate: Real = 0.1
+
+x_neopt1, y_neopt1, num_iterations_neopt1, execution_time_neopt1, trajectory_neopt1 = gradient_descent(f1, grad_f1, x0,
+                                                                                                       y0,
+                                                                                                       golden_section_search,
+                                                                                                       EPS,
+                                                                                                       learning_rate)
 
 print("\nФункция x^2 + y^2 (неоптимальный шаг):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations_neopt1}")
 print(f"Полученная точка: ({x_neopt1}, {y_neopt1})")
 print(f"Полученное значение функции: {f1(x_neopt1, y_neopt1)}")
 print(f"Время работы: {execution_time_neopt1:.4f} сек")
 
-x_opt1, y_opt1, num_iterations1, execution_time1, trajectory1 = gradient_descent(f1, grad_f1, x0, y0, golden_section_search, eps)
+x_opt1, y_opt1, num_iterations1, execution_time1, trajectory1 = gradient_descent(f1, grad_f1, x0, y0,
+                                                                                 golden_section_search, EPS)
 
 print("\nФункция x^2 + y^2 (золотое сечение):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations1}")
 print(f"Полученная точка: ({x_opt1}, {y_opt1})")
 print(f"Полученное значение функции: {f1(x_opt1, y_opt1)}")
 print(f"Время работы: {execution_time1:.4f} сек")
 
-x_opt1_ter, y_opt1_ter, num_iterations1_ter, execution_time1_ter, trajectory1_ter = gradient_descent(f1, grad_f1, x0, y0, ternary_search, eps)
+x_opt1_ter, y_opt1_ter, num_iterations1_ter, execution_time1_ter, trajectory1_ter = gradient_descent(f1, grad_f1, x0,
+                                                                                                     y0, ternary_search,
+                                                                                                     EPS)
 
 print("\nФункция x^2 + y^2 (тернарный поиск):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations1_ter}")
 print(f"Полученная точка: ({x_opt1_ter}, {y_opt1_ter})")
 print(f"Полученное значение функции: {f1(x_opt1_ter, y_opt1_ter)}")
 print(f"Время работы: {execution_time1_ter:.4f} сек")
 
-x_neopt2, y_neopt2, num_iterations_neopt2, execution_time_neopt2, trajectory_neopt2 = gradient_descent(f2, grad_f2, x0, y0, golden_section_search, eps, learning_rate)
+x_neopt2, y_neopt2, num_iterations_neopt2, execution_time_neopt2, trajectory_neopt2 = gradient_descent(f2, grad_f2, x0,
+                                                                                                       y0,
+                                                                                                       golden_section_search,
+                                                                                                       EPS,
+                                                                                                       learning_rate)
 
 print("\nФункция x^2 * y^2 * log(8x^2 + 3y^2) (неоптимальный шаг):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations_neopt2}")
 print(f"Полученная точка: ({x_neopt2}, {y_neopt2})")
 print(f"Полученное значение функции: {f2(x_neopt2, y_neopt2)}")
 print(f"Время работы: {execution_time_neopt2:.4f} сек")
 
-x_opt2, y_opt2, num_iterations2, execution_time2, trajectory2 = gradient_descent(f2, grad_f2, x0, y0, golden_section_search, eps)
+x_opt2, y_opt2, num_iterations2, execution_time2, trajectory2 = gradient_descent(f2, grad_f2, x0, y0,
+                                                                                 golden_section_search, EPS)
 
 print("\nФункция x^2 * y^2 * log(8x^2 + 3y^2) (золотое сечение):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations2}")
 print(f"Полученная точка: ({x_opt2}, {y_opt2})")
 print(f"Полученное значение функции: {f2(x_opt2, y_opt2)}")
 print(f"Время работы: {execution_time2:.4f} сек")
 
-x_opt2_ter, y_opt2_ter, num_iterations2_ter, execution_time2_ter, trajectory2_ter = gradient_descent(f2, grad_f2, x0, y0, ternary_search, eps)
+x_opt2_ter, y_opt2_ter, num_iterations2_ter, execution_time2_ter, trajectory2_ter = gradient_descent(f2, grad_f2, x0,
+                                                                                                     y0, ternary_search,
+                                                                                                     EPS)
 
 print("\nФункция x^2 * y^2 * log(8x^2 + 3y^2) (тернарный поиск):")
-print(f"Критерий останова: |delta f| < {eps}")
+print(f"Критерий останова: |delta f| < {EPS}")
 print(f"Число итераций: {num_iterations2_ter}")
 print(f"Полученная точка: ({x_opt2_ter}, {y_opt2_ter})")
 print(f"Полученное значение функции: {f2(x_opt2_ter, y_opt2_ter)}")
 print(f"Время работы: {execution_time2_ter:.4f} сек")
 
 
-def f1_arr(x):
-    return f1(x[0], x[1])
+def f1_arr(x: NDArray[Real, ...]) -> Real:
+	return f1(x[0], x[1])
 
-def f2_arr(x):
-    return f2(x[0], x[1])
 
-result_f1 = minimize(f1_arr, [x0, y0], method='Nelder-Mead', tol=eps)
-result_f2 = minimize(f2_arr, [x0, y0], method='Nelder-Mead', tol=eps)
+def f2_arr(x: NDArray[Real, ...]) -> Real:
+	return f2(x[0], x[1])
+
+
+result_f1 = minimize(f1_arr, np.array([x0, y0]), method='Nelder-Mead', tol=EPS)
+result_f2 = minimize(f2_arr, np.array([x0, y0]), method='Nelder-Mead', tol=EPS)
 
 print("\nФункция x^2 + y^2:")
 print(f"Полученная точка: ({result_f1.x[0]}, {result_f1.x[1]})")
 print(f"Полученное значение функции: {f2(result_f1.x[0], result_f1.x[1])}")
 
-
 print("\nФункция x^2 * y^2 * log(8x^2 + 3y^2):")
 print(f"Полученная точка: ({result_f2.x[0]}, {result_f2.x[1]})")
 print(f"Полученное значение функции: {f2(result_f2.x[0], result_f2.x[1])}\n")
-
 
 x = np.linspace(-1, 1, 100)
 y = np.linspace(-1, 1, 100)
@@ -110,11 +134,9 @@ Z2 = f2(X, Y)
 
 fig = plt.figure(figsize=(12, 6))
 
-
 ax1 = fig.add_subplot(121, projection='3d')
 ax1.plot_surface(X, Y, Z1, cmap='inferno', alpha=0.7)
 ax1.set_title('f1: x^2 + y^2')
-
 
 ax2 = fig.add_subplot(122)
 ax2.contour(X, Y, Z1, cmap='inferno')
@@ -126,11 +148,9 @@ plt.show()
 
 fig = plt.figure(figsize=(12, 6))
 
-
 ax3 = fig.add_subplot(121, projection='3d')
 ax3.plot_surface(X, Y, Z2, cmap='inferno', alpha=0.7)
 ax3.set_title('f2: x^2 * y^2 * log(8x^2 + 3y^2)')
-
 
 ax4 = fig.add_subplot(122)
 ax4.contour(X, Y, Z2, cmap='inferno')
@@ -140,14 +160,12 @@ ax4.set_title('f2: x^2 * y^2 * log(8x^2 + 3y^2)')
 
 plt.show()
 
-
 df = pd.DataFrame({
-    'Function': ['f1', 'f2'],
-    'x_opt': [x_opt1, x_opt2],
-    'y_opt': [y_opt1, y_opt2],
-    'num_iterations': [num_iterations1, num_iterations2],
-    'execution_time': [execution_time1, execution_time2]
+	'Function': ['f1', 'f2'],
+	'x_opt': [x_opt1, x_opt2],
+	'y_opt': [y_opt1, y_opt2],
+	'num_iterations': [num_iterations1, num_iterations2],
+	'execution_time': [execution_time1, execution_time2]
 })
-
 
 print(df)
